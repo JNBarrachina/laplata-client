d7c5adf648e1f77f990a9ebfba0421c5f792049f
= <=>
2025-06-13 12:13:15 +0200

    edit and delete buttons

---
+++ src/components/atoms/RemovePayment/RemovePayment.jsx
@@ -0,0 +1,11 @@
+import "./RemovePayment.css";
+
+const RemovePayment = () => {
+  return (
+    <button className="btnDelete">
+      <img className="imgDelete" src="/src/assets/delete.svg" alt="Delete" />
+    </button>
+  );
+};
+
+export default RemovePayment;
