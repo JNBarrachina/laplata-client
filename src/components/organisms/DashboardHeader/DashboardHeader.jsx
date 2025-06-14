654847cf49f1b7c5b523daf1efe2d0f9a982e040
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-06-14 16:59:26 +0200

    New slogan and dashboard responsiveness implemented

--- src/components/organisms/DashboardHeader/DashboardHeader.jsx
+++ src/components/organisms/DashboardHeader/DashboardHeader.jsx
@@ -8,14 +8,14 @@ export const DashboardHeader = () => {
             <header className='dashboardHeaderContainer'>
                 <img src="/src/assets/laplata.png" alt="" className='dashboardLogo' />
                 <nav className="dashboardNav">
-                    <Link to="/">
-                        <button className='dashboardHeaderBtn'>
-                            <img className='dashboardHeaderImg' src="/src/assets/useraccount.svg" alt="" />
-                        </button>
-                    </Link>
                     <Link to="/dashboard">
                         <button className='dashboardHeaderBtn'>
                             <img className='dashboardHeaderImg' src="/src/assets/dashboard.svg" alt="" />
+                        </button>
+                    </Link>
+                    <Link to="/">
+                        <button className='dashboardHeaderBtn'>
+                            <img className='dashboardHeaderImg' src="/src/assets/useraccount.svg" alt="" />
                         </button>
                     </Link>
                     <Link to="/">
