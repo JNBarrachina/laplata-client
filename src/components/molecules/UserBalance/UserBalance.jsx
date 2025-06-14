8ed0825561d51de0b26922f36504a03be33875a7
Juan Narros <167420227+JNBarrachina@users.noreply.github.com>
2025-06-14 22:41:12 +0200

    More style changes and fixes, added test data (#7)

--- src/components/molecules/UserBalance/UserBalance.jsx
+++
@@ -1,3 +0,0 @@
-export const UserBalance = () => {
-  return <div>UserBalance</div>;
-};
--- src/components/organisms/PaymentsContainer/PaymentsContainer.jsx
+++ src/components/organisms/PaymentsContainer/PaymentsContainer.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
+
 import { Payment } from "../../molecules/Payment/Payment";
+import registros from "../../../data/datatest.json";
+
 
 import "./PaymentsContainer.css";
 
 export const PaymentsContainer = () => {
+  const [payments, setPayments] = useState(registros);
+
   return (
-    <article className="paymentsContainer">
-      <Payment />
-      <Payment />
-      <Payment />
-      <Payment />
-    </article>
+    <section className="paymentsContainer">
+      {payments.map((payment) => (
+        <Payment key={payment.id} payment={payment} />
+      ))}
+    </section>
   );
 };
