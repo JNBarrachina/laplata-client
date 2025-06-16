47388c166d55d51db0fde19d5acd74dcfec352d9
Juan Narros <167420227+JNBarrachina@users.noreply.github.com>
2025-06-16 13:09:35 +0200

    Add new Transaction (#20)

--- src/components/molecules/NewTransaction/NewTransaction.jsx
+++ src/components/molecules/NewTransaction/NewTransaction.jsx
@@ -5,119 +5,127 @@ import { UserTransactionsContext } from "../../../contexts/UserTransactionsConte
 import "./NewTransaction.css";
 
 export const NewTransaction = ({ dialogRef, modalType, transactionData }) => {
-  const { userTransactionsList, setUserTransactionsList } = useContext(
-    UserTransactionsContext
-  );
+    const { userTransactionsList, setUserTransactionsList } = useContext(
+        UserTransactionsContext
+    );
 
-  const [newTitle, setNewTitle] = useState(transactionData.title);
-  const [newAmount, setNewAmount] = useState(transactionData.amount);
-  const [newCategory, setNewCategory] = useState("expense");
-  const [newDescription, setNewDescription] = useState(
-    transactionData.description
-  );
+    const [newTitle, setNewTitle] = useState(transactionData.title);
+    const [newAmount, setNewAmount] = useState(transactionData.amount);
+    const [newCategory, setNewCategory] = useState("expense");
+    const [newDescription, setNewDescription] = useState(
+        transactionData.description
+    );
 
-  const [msg, setMsg] = useState("");
+    function formatDate(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
 
-  const addTransaction = () => {
-    const newId = userTransactionsList[userTransactionsList.length - 1].id + 1;
+    const addTransaction = () => {
+        const newId = userTransactionsList[userTransactionsList.length - 1].id + 1;
+        const today = new Date();
 
-    const newTransaction = {
-      id: newId,
-      title: newTitle,
-      amount: newAmount,
-      date: Date.now(),
-      type: newCategory,
-      description: newDescription,
+        const newTransaction = {
+            id: newId,
+            title: newTitle,
+            amount: newAmount,
+            date: formatDate(today),
+            type: newCategory,
+            description: newDescription,
+        };
+
+        userTransactionsList.push(newTransaction)
+        setUserTransactionsList([...userTransactionsList]);
+
+        console.log(userTransactionsList)
+        closeModal();
     };
 
-    setUserTransactionsList([...userTransactionsList, newTransaction]);
-    closeModal();
-  };
+    const editTransaction = () => {
+        const editedTransactionData = {
+            id: transactionData.id,
+            name,
+            amount,
+            category,
+            description,
+        };
 
-  const editTransaction = () => {
-    const editedTransactionData = {
-      id: transactionData.id,
-      name,
-      amount,
-      category,
-      description,
+        setUserTransactionsList([...userTransactionsList, editTransaction]);
+
+        closeModal();
     };
 
-    setUserTransactionsList([...userTransactionsList, editTransaction]);
+    const closeModal = () => {
+        dialogRef.current?.close();
+    };
 
-    closeModal();
-  };
+    return (
+        <>
+            <dialog ref={dialogRef} className="newTransactionDialog">
+                <div className="newTransactionDialogContainer">
+                    {modalType == "Create" && (
+                        <h2 className="dialogTitle">New Transaction</h2>
+                    )}
+                    {modalType == "Edit" && (
+                        <h2 className="dialogTitle">Edit Transaction</h2>
+                    )}
 
-  const closeModal = () => {
-    dialogRef.current?.close();
-  };
+                    <form action="" className="newTransactionForm">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            onChange={(e) => setNewTitle(e.target.value)}
+                            value={newTitle}
+                            required
+                        />
+                        <label htmlFor="amount">Amount</label>
+                        <input
+                            type="number"
+                            name="amount"
+                            id="amount"
+                            placeholder={transactionData.amount}
+                            onChange={(e) => setNewAmount(e.target.value)}
+                            value={newAmount}
+                            required
+                        />
+                        <label htmlFor="category">Category</label>
+                        <select
+                            className="newTransactionCategory"
+                            name="category"
+                            id="category"
+                            onChange={(e) => setNewCategory(e.target.value)}
+                        >
+                            <option value="expense">Expense</option>
+                            <option value="income">Income</option>
+                        </select>
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            className="newTransactionDescription"
+                            name="description"
+                            id="description"
+                            placeholder={transactionData.description}
+                            onChange={(e) => setNewDescription(e.target.value)}
+                            value={newDescription}
+                        ></textarea>
+                    </form>
 
-  return (
-    <>
-      <dialog ref={dialogRef} className="newTransactionDialog">
-        <div className="newTransactionDialogContainer">
-          {modalType == "Create" && (
-            <h2 className="dialogTitle">New Transaction</h2>
-          )}
-          {modalType == "Edit" && (
-            <h2 className="dialogTitle">Edit Transaction</h2>
-          )}
-
-          <form action="" className="newTransactionForm">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              onChange={(e) => setNewTitle(e.target.value)}
-              value={newTitle}
-              required
-            />
-            <label htmlFor="amount">Amount</label>
-            <input
-              type="number"
-              name="amount"
-              id="amount"
-              placeholder={transactionData.amount}
-              onChange={(e) => setNewAmount(e.target.value)}
-              value={newAmount}
-              required
-            />
-            <label htmlFor="category">Category</label>
-            <select
-              className="newTransactionCategory"
-              name="category"
-              id="category"
-              onChange={(e) => setNewCategory(e.target.value)}
-            >
-              <option value="expense">Expense</option>
-              <option value="income">Income</option>
-            </select>
-            <label htmlFor="description">Description</label>
-            <textarea
-              className="newTransactionDescription"
-              name="description"
-              id="description"
-              placeholder={transactionData.description}
-              onChange={(e) => setNewDescription(e.target.value)}
-              value={newDescription}
-            ></textarea>
-            <p>{msg}</p>
-          </form>
-
-          <div className="newTransactionBtns">
-            <button className="backBtn" onClick={closeModal}>
-              Back
-            </button>
-            {modalType == "Create" && (
-              <button onClick={addTransaction}>Add Transaction</button>
-            )}
-            {modalType == "Edit" && (
-              <button onClick={editTransaction}>Edit Transaction</button>
-            )}
-          </div>
-        </div>
-      </dialog>
-    </>
-  );
+                    <div className="newTransactionBtns">
+                        <button className="backBtn" onClick={closeModal}>
+                            Back
+                        </button>
+                        {modalType == "Create" && (
+                            <button onClick={addTransaction}>Add Transaction</button>
+                        )}
+                        {modalType == "Edit" && (
+                            <button onClick={editTransaction}>Edit Transaction</button>
+                        )}
+                    </div>
+                </div>
+            </dialog>
+        </>
+    );
 };
